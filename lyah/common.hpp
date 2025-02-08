aaf27e1b08f24f22f62a3e30336b88c97e092618
matteokeole <matteoo34@icloud.com>
2025-02-08 20:47:55 +0100

    feat: Add scalar abs function

--- lyah/common.hpp
+++ lyah/common.hpp
@@ -6,6 +6,8 @@
 #include "base.hpp"
 
 namespace lyah {
+	using std::abs;
+
 	template<std::size_t C, typename T>
 	LYAH_NODISCARD LYAH_INLINE vec<C, T> LYAH_CALL lerp(vec<C, T> a, vec<C, T> b, T t) {
 		return a * (static_cast<T>(1) - t) + b * t;
