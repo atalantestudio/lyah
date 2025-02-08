27b62daabc50111658f106b841b1b04d199b4a9b
matteokeole <matteoo34@icloud.com>
2025-02-08 19:07:25 +0100

    feat: Add vec<3, std::float_t> with base operations

--- lyah/vec.hpp
+++ lyah/vec.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
 #include "vec/vec2.hpp"
+#include "vec/vec3.hpp"
 
 #include "vec/m128/base.inl"
--- lyah/vec/vec3.hpp
+++ lyah/vec/vec3.hpp
@@ -6,20 +6,20 @@
 
 namespace lyah {
 	template<typename T>
-	struct vec<2, T> {
-		using __m_t = typename internal::vec_t<2, T>::__m_t;
+	struct vec<3, T> {
+		using __m_t = typename internal::vec_t<3, T>::__m_t;
 
 		__m_t m;
 
 		LYAH_NODISCARD vec();
-		LYAH_NODISCARD vec(T x, T y);
+		LYAH_NODISCARD vec(T x, T y, T z);
 		LYAH_NODISCARD LYAH_CONSTEXPR explicit vec(__m_t m);
 
 		template<typename U>
-		LYAH_NODISCARD explicit vec(vec<2, U> a) : m(internal::convert<typename vec<2, U>::__m_t, __m_t>(a.m)) {}
+		LYAH_NODISCARD explicit vec(vec<3, U> a) : m(internal::convert<typename vec<3, U>::__m_t, __m_t>(a.m)) {}
 
 		LYAH_NODISCARD T LYAH_CALL operator [](std::size_t index) const LYAH_NOEXCEPT;
 	};
 }
 
-#include "vec/vec2_m128.inl"
+#include "vec/vec3_m128.inl"
