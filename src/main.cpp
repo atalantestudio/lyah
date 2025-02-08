ef9ba066c7e2ff7f6c070739f2ab21facf3daf75
matteokeole <matteoo34@icloud.com>
2025-02-08 19:16:43 +0100

    feat: Add lyah::vec<2, std::float_t> base operation tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "pch.hpp"
 
 #include "test/constants.hpp"
+#include "test/vec2_m128.hpp"
 
 int main() {
 	test::Logger::initialize();
 
 	constants::runAll();
+	vec2_m128::runAll();
 
 	return 0;
 }
