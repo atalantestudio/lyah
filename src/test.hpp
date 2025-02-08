1e1b907c1115912db949135d82753f81e6e56129
matteokeole <matteoo34@icloud.com>
2025-02-08 19:14:35 +0100

    feat: Check lyah::vec<C, T> contents

--- src/test.hpp
+++ src/test.hpp
@@ -30,4 +30,45 @@ namespace test {
 
 		return true;
 	}
+
+	template<std::size_t C>
+	inline bool eq(const lyah::vec<C, std::float_t>& a, const lyah::vec<C, std::float_t>& b, std::float_t precision = 0.0f) {
+		const std::float_t* bufferA = a.m.m128_f32;
+		const std::float_t* bufferB = b.m.m128_f32;
+
+		for (std::size_t i = 0; i < 4; i++) {
+			if (std::abs(bufferA[i] - bufferB[i]) > precision) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	inline bool eq(const lyah::vec<2, std::double_t>& a, const lyah::vec<2, std::double_t>& b, std::double_t precision = 0.0) {
+		const std::double_t* bufferA = a.m.m128d_f64;
+		const std::double_t* bufferB = b.m.m128d_f64;
+
+		for (std::size_t i = 0; i < 2; i++) {
+			if (std::abs(bufferA[i] - bufferB[i]) > precision) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	template<std::size_t C>
+	inline bool eq(const lyah::vec<C, std::double_t>& a, const lyah::vec<C, std::double_t>& b, std::double_t precision = 0.0) {
+		const std::double_t* bufferA = a.m.m256d_f64;
+		const std::double_t* bufferB = b.m.m256d_f64;
+
+		for (std::size_t i = 0; i < 4; i++) {
+			if (std::abs(bufferA[i] - bufferB[i]) > precision) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
