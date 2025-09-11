0c97a913ad87d4f528ba45125c5e3ba19b318ae4
matteokeole <matteoo34@icloud.com>
2025-09-11 20:20:13 +0200

    fix: Remove constexpr attribute on quat constructor

--- lyah/quat/quat.hpp
+++ lyah/quat/quat.hpp
@@ -20,7 +20,7 @@ namespace lyah {
 		LYAH_NODISCARD quat();
 		LYAH_NODISCARD quat(T w, T x, T y, T z);
 
-		LYAH_NODISCARD LYAH_CONSTEXPR explicit quat(__m_t m);
+		LYAH_NODISCARD explicit quat(__m_t m);
 
 		template<typename U>
 		LYAH_NODISCARD quat(quat<U> a);
