e4e7187426311bb8be08bc9242b9727c4d56e5bd
matteokeole <matteoo34@icloud.com>
2026-07-17 21:44:49 +0200

    ci: Update copyright license header validation script

--- scripts/check_copyright_license_headers.sh
+++ scripts/check_copyright_license_headers.sh
@@ -4,13 +4,13 @@ files=$(git diff --diff-filter=d --name-only origin/$GITHUB_BASE_REF origin/$GIT
 error_files=()
 
 for file in $files; do
-	if [[ $(head -n 2 $file) != "$HEADER" ]]; then
+	if [[ $(sed -n '1p' $file) != "$HEADER_COPYRIGHT_START"* || $(sed -n '2p' $file) != "$HEADER_LICENSE_START"* ]]; then
 		error_files+=($file)
 	fi
 done
 
 if [[ ${#error_files[@]} -gt 0 ]]; then
-	echo "The following file(s) don't start with a copyright license header." >> $GITHUB_STEP_SUMMARY
+	echo "The following files don't start with a copyright license header." >> $GITHUB_STEP_SUMMARY
 
 	for file in "${error_files[@]}"; do
 		echo "- [$file]($GITHUB_TREE_URL/$file)" >> $GITHUB_STEP_SUMMARY
